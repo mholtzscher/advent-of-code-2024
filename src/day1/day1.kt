e0101bacbf6b5bd745d569d545e561c4cb459cf0
Michael Holtzscher <michael@holtzscher.com>
2024-12-01 13:33:51 -0600

    impl day 1 part 1

---
+++ src/day1/day1.kt
@@ -0,0 +1,26 @@
+package day1
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val left: MutableList<Int> = mutableListOf()
+    val right: MutableList<Int> = mutableListOf()
+
+    File("src/day1/input.txt").readLines().map { line ->
+        val (l, r) = line.split("   ").map { it.toInt() }
+        left.add(l)
+        right.add(r)
+    }
+    left.sort()
+    right.sort()
+
+    part1(left, right)
+}
+
+fun part1(left: MutableList<Int>, right: MutableList<Int>) {
+    var sum = 0
+    for (i in left.indices) sum += abs(right[i] - left[i])
+
+    println(sum)
+}
