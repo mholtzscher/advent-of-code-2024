53dd7c36d8cd27248c4c4cd42976047ca69e947d
Michael Holtzscher <michael@holtzscher.com>
2024-12-01 13:52:43 -0600

    alternate solution to day 1 part 1

--- src/day1/day1.kt
+++ src/day1/day1.kt
@@ -3,24 +3,43 @@ package day1
 import java.io.File
 import kotlin.math.abs
 
+//fun main() {
+//    val left: MutableList<Int> = mutableListOf()
+//    val right: MutableList<Int> = mutableListOf()
+//
+//    File("src/day1/input.txt").readLines().map { line ->
+//        val (l, r) = line.split("   ").map { it.toInt() }
+//        left.add(l)
+//        right.add(r)
+//    }
+//    left.sort()
+//    right.sort()
+//
+//    part1(left, right)
+//}
+//
+//fun part1(left: MutableList<Int>, right: MutableList<Int>) {
+//    var sum = 0
+//    for (i in left.indices) sum += abs(right[i] - left[i])
+//
+//    println(sum)
+//}
+
+
 fun main() {
-    val left: MutableList<Int> = mutableListOf()
-    val right: MutableList<Int> = mutableListOf()
+    val (left, right) = File("src/day1/input.txt")
+        .readLines()
+        .map { line ->
+            line.split("   ")
+                .map { it.toInt() }
+                .let { (l, r) -> l to r }
+        }
+        .unzip()
 
-    File("src/day1/input.txt").readLines().map { line ->
-        val (l, r) = line.split("   ").map { it.toInt() }
-        left.add(l)
-        right.add(r)
-    }
-    left.sort()
-    right.sort()
+    val sortedLeft = left.sorted()
+    val sortedRight = right.sorted()
 
-    part1(left, right)
-}
-
-fun part1(left: MutableList<Int>, right: MutableList<Int>) {
-    var sum = 0
-    for (i in left.indices) sum += abs(right[i] - left[i])
-
-    println(sum)
+    val absoluteDifferenceSum = sortedLeft.zip(sortedRight)
+        .sumOf { (l, r) -> abs(r - l) }
+    println(absoluteDifferenceSum)
 }
