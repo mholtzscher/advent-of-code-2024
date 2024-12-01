a4af40495a96f25937a106fc6a47c4b5efcecc1a
Michael Holtzscher <michael@holtzscher.com>
2024-12-01 14:21:08 -0600

    impl day 1 part 2

--- src/day1/day1.kt
+++ src/day1/day1.kt
@@ -3,9 +3,11 @@ package day1
 import java.io.File
 import kotlin.math.abs
 
-fun main(){
+fun main() {
     part1()
+    part2()
 }
+
 fun part1() {
     val (left, right) = File("src/day1/input.txt")
         .readLines()
@@ -22,4 +24,19 @@ fun part1() {
     val absoluteDifferenceSum = sortedLeft.zip(sortedRight)
         .sumOf { (l, r) -> abs(r - l) }
     println(absoluteDifferenceSum)
+}
+
+
+fun part2() {
+    val (left, right) = File("src/day1/input.txt")
+        .readLines()
+        .map { line ->
+            line.split("   ")
+                .map { it.toInt() }
+                .let { (l, r) -> l to r }
+        }
+        .unzip()
+
+    val sum = left.sumOf { l -> right.count { r -> r == l } * l }
+    println(sum)
 }
