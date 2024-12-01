54e110e033ca8cc6c5351f21f7f5344524109e7f
Michael Holtzscher <michael@holtzscher.com>
2024-12-01 13:54:19 -0600

    cleanup comments

--- src/day1/day1.kt
+++ src/day1/day1.kt
@@ -3,30 +3,10 @@ package day1
 import java.io.File
 import kotlin.math.abs
 
-//fun main() {
-//    val left: MutableList<Int> = mutableListOf()
-//    val right: MutableList<Int> = mutableListOf()
-//
-//    File("src/day1/input.txt").readLines().map { line ->
-//        val (l, r) = line.split("   ").map { it.toInt() }
-//        left.add(l)
-//        right.add(r)
-//    }
-//    left.sort()
-//    right.sort()
-//
-//    part1(left, right)
-//}
-//
-//fun part1(left: MutableList<Int>, right: MutableList<Int>) {
-//    var sum = 0
-//    for (i in left.indices) sum += abs(right[i] - left[i])
-//
-//    println(sum)
-//}
-
-
-fun main() {
+fun main(){
+    part1()
+}
+fun part1() {
     val (left, right) = File("src/day1/input.txt")
         .readLines()
         .map { line ->
